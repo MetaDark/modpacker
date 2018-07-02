7c660c60b24c47fdece53a7d10376801f8b1f992
MetaDark <kira.bruneau@gmail.com>
2018-07-02 19:57:40 -0400

    Added Url type

--- modpack.py
+++ modpack.py
@@ -3,32 +3,34 @@
 from abc import ABC, abstractmethod
 from bs4 import BeautifulSoup, Tag
 from itertools import takewhile
-from typing import Dict, List, Optional
+from typing import NewType, Dict, List, Optional
 from urllib.parse import quote as urlquote, urljoin
 import re
 import requests
 
+Url = NewType('Url', str)
+
 # Similar to urljoin, but constructs url from path segments instead of relative url
-def urlpath(base: str, *segments: str):
-    base = base.rstrip('/')
+def urlpath(base: Url, *segments: str) -> Url:
+    base = Url(base.rstrip('/'))
     url = '/'.join((urlquote(segment, safe='') for segment in segments))
-    return '{}/{}'.format(base, url)
+    return Url('{}/{}'.format(base, url))
 
 class Mod(ABC):
     @abstractmethod
-    def url(self) -> str:
+    def url(self) -> Url:
         pass
 
-    def doc(self) -> str:
+    def doc(self) -> Url:
         return self.url()
 
     @abstractmethod
-    def latest(self, mc_version: str) -> List[str]:
+    def latest(self, mc_version: str) -> List[Url]:
         pass
 
 class Repo(ABC):
     @abstractmethod
-    def url(self) -> str:
+    def url(self) -> Url:
         pass
 
     @abstractmethod
@@ -38,8 +40,8 @@ class Repo(ABC):
 class CurseForge(Repo):
     mc_versions: Optional[Dict[str, str]] = None
 
-    def url(self) -> str:
-        return 'https://minecraft.curseforge.com'
+    def url(self) -> Url:
+        return Url('https://minecraft.curseforge.com')
 
     def mod(self, mod: str) -> Mod:
         return CurseForgeMod(self, mod)
@@ -61,18 +63,18 @@ class CurseForgeMod(Mod):
         self.curse_forge = curse_forge
         self.mod = mod
 
-    def url(self) -> str:
+    def url(self) -> Url:
         return urlpath(self.curse_forge.url(), 'projects', self.mod)
 
-    def doc(self) -> str:
+    def doc(self) -> Url:
         url = self.url()
         res = requests.get(url)
         page = BeautifulSoup(res.text, 'lxml')
-        return next((urljoin(url, link.get('href'))
+        return next((Url(urljoin(url, link.get('href')))
                      for link in page.find(class_='e-menu').find_all('a')
                      if link.string.strip() == 'Wiki'), url)
 
-    def latest(self, mc_version: str) -> List[str]:
+    def latest(self, mc_version: str) -> List[Url]:
         url = urlpath(self.url(), 'files')
         res = requests.get(url, params = {
             'filter-game-version': self.curse_forge.resolve_mc_version(mc_version),
@@ -86,13 +88,13 @@ class CurseForgeMod(Mod):
         for row in page.find('table', class_='listing-project-file').find_all('tr'):
             if row.find(class_='release-phase'):
                 # TODO: Grab dependencies
-                return [urljoin(url, row.find(class_='project-file-download-button').find('a').get('href'))]
+                return [Url(urljoin(url, row.find(class_='project-file-download-button').find('a').get('href')))]
 
         raise LookupError('\'{}\' doesn\'t have a release for minecraft {}'.format(self.mod, mc_version))
 
 class Micdoodle8(Repo):
-    def url(self) -> str:
-        return 'https://micdoodle8.com'
+    def url(self) -> Url:
+        return Url('https://micdoodle8.com')
 
     def mod(self, mod: str) -> Mod:
         mods = {
@@ -108,13 +110,13 @@ class Galacticraft(Mod):
     def __init__(self, micdoodle8: Micdoodle8) -> None:
         self.micdoodle8 = micdoodle8
 
-    def url(self) -> str:
+    def url(self) -> Url:
         return urlpath(self.micdoodle8.url(), 'mods', 'galacticraft')
 
-    def doc(self) -> str:
-        return 'https://wiki.micdoodle8.com/wiki/Galacticraft'
+    def doc(self) -> Url:
+        return Url('https://wiki.micdoodle8.com/wiki/Galacticraft')
 
-    def latest(self, mc_version: str) -> List[str]:
+    def latest(self, mc_version: str) -> List[Url]:
         url = urlpath(self.url(), 'downloads')
         res = requests.get(url)
         page = BeautifulSoup(res.text, 'lxml')
@@ -130,50 +132,50 @@ class Galacticraft(Mod):
         except StopIteration:
             raise LookupError('\'{}\' doesn\'t have a release for minecraft {}'.format('galacticraft', mc_version))
 
-    def resolve_download_section(self, downloads: Tag, section: str) -> List[str]:
+    def resolve_download_section(self, downloads: Tag, section: str) -> List[Url]:
         # TODO: Support scraping links from 'Latest' sections
         links = downloads.find('h4', string=section).find_next_siblings('a')
         links = takewhile(lambda elem: elem.name != 'h4', links)
-        return [link.get('href') for link in links]
+        return [Url(link.get('href')) for link in links]
 
-    def resolve_download_url(self, url: str) -> str:
+    def resolve_download_url(self, url: Url) -> Url:
         res = requests.get(url)
-        return re.search(r'var phpStr = "(.*?)"', res.text)[1]
+        return Url(re.search(r'var phpStr = "(.*?)"', res.text)[1])
 
 class OptiFine(Mod):
-    def url(self) -> str:
-        return 'https://optifine.net'
+    def url(self) -> Url:
+        return Url('https://optifine.net')
 
-    def doc(self) -> str:
-        return 'https://github.com/sp614x/optifine/tree/master/OptiFineDoc/doc'
+    def doc(self) -> Url:
+        return Url('https://github.com/sp614x/optifine/tree/master/OptiFineDoc/doc')
 
-    def latest(self, mc_version: str) -> List[str]:
+    def latest(self, mc_version: str) -> List[Url]:
         url = urlpath(self.url(), 'downloads')
         res = requests.get(url)
         page = BeautifulSoup(res.text, 'lxml')
         download = self.resolve_download_section(page, mc_version)
         return [self.resolve_download_url(url) for url in download]
 
-    def resolve_download_section(self, page: Tag, mc_version: str) -> List[str]:
+    def resolve_download_section(self, page: Tag, mc_version: str) -> List[Url]:
         version_header = page.find(class_='downloads').find('h2', string='Minecraft {}'.format(mc_version))
         if not version_header:
             raise LookupError('\'optifine\' doesn\'t have a release for minecraft {}'.format(mc_version))
 
         downloads = version_header.find_next_sibling(class_='downloadTable')
-        return [downloads.find(class_='downloadLineMirror').find('a').get('href')]
+        return [Url(downloads.find(class_='downloadLineMirror').find('a').get('href'))]
 
-    def resolve_download_url(self, url: str) -> str:
+    def resolve_download_url(self, url: Url) -> Url:
         res = requests.get(url)
-        return urljoin(url, BeautifulSoup(res.text, 'lxml').find(id='Download').find('a').get('href'))
+        return Url(urljoin(url, BeautifulSoup(res.text, 'lxml').find(id='Download').find('a').get('href')))
 
 class PixelmonReforged(Mod):
-    def url(self) -> str:
-        return 'https://reforged.gg'
+    def url(self) -> Url:
+        return Url('https://reforged.gg')
 
-    def doc(self) -> str:
-        return 'https://pixelmonmod.com/wiki'
+    def doc(self) -> Url:
+        return Url('https://pixelmonmod.com/wiki')
 
-    def latest(self, mc_version: str) -> List[str]:
+    def latest(self, mc_version: str) -> List[Url]:
         if mc_version != '1.12.2':
             raise LookupError('unsupported minecraft version: {}'.format(mc_version))
 
@@ -182,7 +184,7 @@ class PixelmonReforged(Mod):
         page = BeautifulSoup(res.text, 'lxml')
 
         # TODO: Bypass adfly
-        return [urljoin(url, page.find('a', 'download').get('href'))]
+        return [Url(urljoin(url, page.find('a', 'download').get('href')))]
 
 # Examples:
 curse_forge = CurseForge()
